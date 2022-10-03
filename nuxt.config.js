7d8a8b160f41c46c0ab3f5a3b9c69546c99af8b3
Emilly Park <jjung4684@gmail.com>
2022-10-03 15:08:03 +0900

    메타태그 테스트

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,16 +1,17 @@
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    target : 'static', //SSG 배포 방식
+    // target : 'static', //SSG 배포 방식
     // target : 'server', //SSR 배포 방식
-    title: 'learn-nuxt',
+
+    title: 'Nuxt Shopping',
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Nuxt Shopping Example' },
       { name: 'format-detection', content: 'telephone=no' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
